06e303056d5c81870d47716cdacfd286fd61e276
Christopher Bosserman <christopherpbosserman@gmail.com>
2021-04-19 19:13:46 -0400

    boilerplate

--- client/App.jsx
+++ client/App.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    console.log('popppppp');
     return (
       <div>
         <h1>Hello World</h1>
--- server/server.js
+++ server/server.js
@@ -5,12 +5,12 @@ const app = express();
 
 const PORT = 3000;
 
-// app.use('/build', express.static(path.resolve(__dirname, '../build')));
+app.use('/build', express.static(path.resolve(__dirname, '..', 'build')));
 
 app.get('/', (req, res) => {
-  return res.status(200).sendFile(path.resolve(__dirname, '../index.html'));
-})
+  return res.status(200).sendFile(path.resolve(__dirname, '..', 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}...`);
-})
+});
