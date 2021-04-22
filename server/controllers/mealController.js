034ff103ce62640dbbdc783dcab7f18c30eb8724
Christopher Bosserman <christopherpbosserman@gmail.com>
2021-04-22 09:03:55 -0400

    returning ids now

--- server/controllers/mealController.js
+++ server/controllers/mealController.js
@@ -25,7 +25,8 @@ mealController.createMeal = (req, res, next) => {
 mealController.getMeals = (req, res, next) => {
   console.log('mealController.getMeals');
   const { userid } = req.cookies;
-  const text = 'SELECT name, description FROM meals WHERE chef_id = ($1)';
+  const text =
+    'SELECT meal._id AS meadID, name, description FROM meals WHERE chef_id = ($1)';
   const values = [userid];
   db.query(text, values)
     .then((resp) => {
